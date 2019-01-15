31f4832293072d6fca4384ae8e4f099b1f583085
LMettrop <lmettrop@gmail.com>
2019-01-15 17:17:21 +0100

    Update on visualisation

--- Code Lennart/car.py
+++ Code Lennart/car.py
@@ -1,6 +1,7 @@
 from mesa import Model
 from mesa import Agent
 import random
+import matplotlib.pyplot as plt
 
 class Car(Agent):
     def __init__(self, unique_id, size=1, start_lane=0, speed=100):
@@ -9,8 +10,12 @@ class Car(Agent):
         self.start_lane = start_lane
         self.x = 0
         self.y = start_lane + 0.5
+        self.pos = (self.x, self.y)
         self.speed = speed
         self.new_x = 0
+        colormap = plt.get_cmap('Dark2')
+        r = random.random()
+        self.color = colormap(r)
 
     def step(self):
         self.new_x = self.x + self.speed
