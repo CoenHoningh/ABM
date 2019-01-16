c032dca70998ab3fc2835e6e28ca3907fc934ddf
coen <coenhoningh@gmail.com]>
2019-01-16 19:02:44 +0100

    inhalen zou nu goed moeten werken, soms komen ze vast te zitten tough

--- Code Lennart/cargrid.py
+++ Code Lennart/cargrid.py
@@ -4,7 +4,7 @@ import random
 import matplotlib.pyplot as plt
 
 class Car(Agent):
-    def __init__(self, unique_id, model, size=1, start_lane=0, speed=100):
+    def __init__(self, unique_id, model, size=1, start_lane=0, speed=1):
         super().__init__(unique_id, model)
         self.unique_id = unique_id
         self.size = size
@@ -22,25 +22,40 @@ class Car(Agent):
         self.color = self.colormap(r)
 
     def _is_free(self, _view, _lane):
+        '''
+        Checks is the lane is free.
+            _view: how many places to look ahead
+            _lane: which lane to check: -1 = right, 0 = same, 1 = left
+        '''
         bool_list = [self.model.grid.is_cell_empty((self.x+x, self.y+_lane)) for x in range(1, _view)]
         return all(bool_list)
 
     def step(self):
         self.pos = (self.x, self.y)
-        if self._is_free(8, 0):
-            self.speed = min(self.max_speed, self.speed+1)
+        self.speed = min(self.max_speed, self.speed+1)
+        if self._is_free(self.speed+1, 0):
+            '''
+            Move ahead if the current speed allows
+            '''
             self.new_x = self.x + self.speed
             if self.y > 0:
-                if self._is_free(10, -1):
+                if self._is_free(self.speed+3, -1):
+                    '''
+                    Move a lane to the right if speed allows
+                    '''
                     self.new_y = self.y - 1
 
-        elif self.y < (self.model.lanes-1):
-            if self._is_free(6, 1):
-                self.speed = min(self.max_speed, self.speed+1)
-                self.new_x = self.x + self.speed
-                self.new_y = self.y + 1
+        elif self.y < (self.model.lanes-1) and self._is_free(self.speed+1, 1):
+            '''
+            Move a lane to the left if the speed allows
+            '''
+            self.new_x = self.x + self.speed
+            self.new_y = self.y + 1
 
         else:
+            '''
+            Slow down 1 tick if none are possible
+            '''
             self.speed = max(self.speed-1, 0)
             self.new_x = self.x + self.speed
             self.new_y = self.y
@@ -60,7 +75,10 @@ class Car(Agent):
         self.x = self.new_x
         self.y = self.new_y
         print('yo')
-        self.model.grid.move_agent(self, (self.x, self.y))
+        if self.model.grid.is_cell_empty((self.x, self.y)):
+            self.model.grid.move_agent(self, (self.x, self.y))
+        else:
+            print()
 
     def advance(self):
         if self.model.grid.out_of_bounds((self.new_x+10, self.new_y)):
