3f610f65565405981a6fe10bcac974b5b402c184
coen <coenhoningh@gmail.com>
2019-01-21 02:02:42 +0100

    Refactor van de meeste code, moet nu wat beter leesbaar zijn en wat sneller draaien.

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,12 +1,21 @@
+"""
+Module for data collection and processing routines.
+"""
 import numpy as np
 
 
 def avg_speed(model):
+    """
+    Computes the average speed of all cars.
+    """
     speeds = [agent.speed for agent in model.schedule.agents]
     return np.average(speeds)
 
 
 def lane_speeds(model):
+    """
+    Computes the average speed of all cars per lane.
+    """
     speeds = [[]]*model.lanes
     for agent in model.schedule.agents:
         speeds[agent.lane].append(agent.speed)
