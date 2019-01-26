1de44feb07f10494f15fa2130c07d9485d2ab550
coen <coenhoningh@gmail.com>
2019-01-26 21:47:32 +0100

    Alles geimplementeerd, nu nog bugjes fixen

--- src/data_collection.py
+++ src/data_collection.py
@@ -9,7 +9,7 @@ def avg_speed(model):
     Computes the average speed of all cars.
     """
     speeds = [agent.speed for agent in model.schedule.agents]
-    return np.average(speeds)*3.6*model.gridsize
+    return np.average(speeds)*3.6
 
 
 def lane_speeds(model):
@@ -18,7 +18,7 @@ def lane_speeds(model):
     """
     speeds = [[] for a in range(model.lanes)]
     for agent in model.schedule.agents:
-        speeds[agent.y].append(agent.speed)
+        speeds[agent.pos[1]].append(agent.speed)
     return [np.average(x)*3.6*model.gridsize for x in speeds]
 
 
--- src/run.py
+++ src/run.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 from modelgrid import RoadSim
 
 tot_time = 20000
-yo = RoadSim(lanes=3, length=5, gridsize=0.1, spawn=0.4, speed=100,
+yo = RoadSim(lanes=3, length=5000, spawn=0.4, speed=100,
              sim_time=tot_time, init_time=1000)
 for a in tqdm(range(tot_time)):
     yo.step()
