0f24efc7dbb7e891f6f5b516ff9780cec8977b5b
coen <coenhoningh@gmail.com>
2019-02-10 20:51:29 +0100

    bla

--- src/SA.py
+++ src/SA.py
@@ -12,70 +12,7 @@ from IPython.display import clear_output
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from itertools import combinations
-import seaborn as sns
 from matplotlib import rcParams
-
-sns.set()
-rcParams.update({'figure.autolayout': True})
-
-# Variable parameters
-problem = {'num_vars': 3,
-           'names': ['spawn', 'agression', 'min_gap'],
-           'bounds': [[0.3, 0.8], [0.2, 0.8], [0.5, 2.0]]}
-
-replicates = 5
-max_steps = 5000
-distinct_samples = 5
-
-param_values = saltelli.sample(problem, distinct_samples)
-
-model_reporters={'Final_avg_speed': avg_speed,
-                 'Final_Cars_in_lane': cars_in_lane,
-                 'Data Collector': lambda m: m.datacollector,
-                 'Total_Avg_speed': avg_speed,
-                 'Total_Cars_in_lane': cars_in_lane,
-                 'Variance_speed': avg_speed,
-                 'Variance_car': cars_in_lane}
-
-batch = BatchRunnerMP(RoadSim,
-                      nr_processes=8,
-                      max_steps=max_steps,
-                      variable_parameters={name:[] for name in problem['names']},
-                      model_reporters=model_reporters)
-
-count = 0
-for i in tqdm(range(replicates)):
-    for vals in tqdm(param_values):
-        vals = list(vals)
-        variable_parameters = {}
-        for name, val in zip(problem['names'], vals):
-            variable_parameters[name] = val
-        batch.run_iteration(variable_parameters, tuple(vals), count)
-        count += 1
-
-data_original = batch.get_model_vars_dataframe()
-data = data_original.copy()
-print(data.shape)
-
-for i in tqdm(range(len(data["Data Collector"]))):
-    if isinstance(data["Data Collector"][i], DataCollector):
-        data_speed = data["Data Collector"][i].get_model_vars_dataframe()['Avg_speed']
-        data_cars = data["Data Collector"][i].get_model_vars_dataframe()['Cars_in_lane']
-        tenproc = int(0.2 * (len(data_speed)))
-        data['Total_Avg_speed'][i] = np.average(data_speed[tenproc:])
-        data['Total_Cars_in_lane'][i] = np.average(data_cars[tenproc:])
-        data['Variance_speed'][i] = np.var(data_speed[tenproc:])
-        data['Variance_car'][i] = np.var(data_cars[tenproc:])
-
-
-data.to_csv('Sobol_result.csv', sep=',', index=False)
-
-print(data)
-
-Si_Speed = sobol.analyze(problem, data['Total_Avg_speed'].as_matrix(), print_to_console=False)
-print("\n")
-Si_Cars = sobol.analyze(problem, data['Total_Cars_in_lane'].as_matrix(), print_to_console=False)
-
 
 def plot_index(s, params, i, title=''):
     """
@@ -110,21 +47,87 @@ def plot_index(s, params, i, title=''):
     plt.errorbar(indices, range(l), xerr=errors, linestyle='None', marker='o')
     plt.axvline(0, c='k')
     fig = plt.gcf()
-    fig.set_size_inches(8, 5)
+    fig.set_size_inches(4, 3)
 
-typename = ["Average_speed", "Number_of_cars"]
-for i, Si in enumerate((Si_Speed, Si_Cars)):
-    # First order
-    plot_index(Si, problem['names'], '1', 'First order sensitivity  -  ' + typename[i])
-    plt.savefig('plots/First_order_sensitivity_|_' + typename[i] + '.png')
-    plt.clf()
 
-    # Second order
-    plot_index(Si, problem['names'], '2', 'Second order sensitivity  -  ' + typename[i])
-    plt.savefig('plots/Second_order_sensitivity_|_' + typename[i] + '.png')
-    plt.clf()
-
-    # Total order
-    plot_index(Si, problem['names'], 'T', 'Total order sensitivity  -  ' + typename[i])
-    plt.savefig('plots/Total_order_sensitivity_|_' + typename[i] + '.png')
-    plt.clf()
+if __name__ == '__main__':
+    import seaborn as sns
+    sns.set()
+    rcParams.update({'figure.autolayout': True})
+    
+    # Variable parameters
+    problem = {'num_vars': 3,
+               'names': ['spawn', 'agression', 'min_gap'],
+               'bounds': [[0.3, 0.8], [0.2, 0.8], [0.5, 2.0]]}
+    
+    replicates = 5
+    max_steps = 5000
+    distinct_samples = 5
+    
+    param_values = saltelli.sample(problem, distinct_samples)
+    
+    model_reporters={'Final_avg_speed': avg_speed,
+                     'Final_Cars_in_lane': cars_in_lane,
+                     'Data Collector': lambda m: m.datacollector,
+                     'Total_Avg_speed': avg_speed,
+                     'Total_Cars_in_lane': cars_in_lane,
+                     'Variance_speed': avg_speed,
+                     'Variance_car': cars_in_lane}
+    
+    batch = BatchRunnerMP(RoadSim,
+                          nr_processes=8,
+                          max_steps=max_steps,
+                          variable_parameters={name:[] for name in problem['names']},
+                          model_reporters=model_reporters)
+    
+    count = 0
+    for i in tqdm(range(replicates)):
+        for vals in tqdm(param_values):
+            vals = list(vals)
+            variable_parameters = {}
+            for name, val in zip(problem['names'], vals):
+                variable_parameters[name] = val
+            batch.run_iteration(variable_parameters, tuple(vals), count)
+            count += 1
+    
+    data_original = batch.get_model_vars_dataframe()
+    data = data_original.copy()
+    print(data.shape)
+    
+    for i in tqdm(range(len(data["Data Collector"]))):
+        if isinstance(data["Data Collector"][i], DataCollector):
+            data_speed = data["Data Collector"][i].get_model_vars_dataframe()['Avg_speed']
+            data_cars = data["Data Collector"][i].get_model_vars_dataframe()['Cars_in_lane']
+            tenproc = int(0.2 * (len(data_speed)))
+            data['Total_Avg_speed'][i] = np.average(data_speed[tenproc:])
+            data['Total_Cars_in_lane'][i] = np.average(data_cars[tenproc:])
+            data['Variance_speed'][i] = np.var(data_speed[tenproc:])
+            data['Variance_car'][i] = np.var(data_cars[tenproc:])
+    
+    
+    data.to_csv('Sobol_result.csv', sep=',', index=False)
+    
+    print(data)
+    
+    Si_Speed = sobol.analyze(problem, data['Total_Avg_speed'].as_matrix(), print_to_console=False)
+    print("\n")
+    Si_Cars = sobol.analyze(problem, data['Total_Cars_in_lane'].as_matrix(), print_to_console=False)
+    
+    
+    
+    typename = ["Average_speed", "Number_of_cars"]
+    for i, Si in enumerate((Si_Speed, Si_Cars)):
+        # First order
+        plot_index(Si, problem['names'], '1', 'First order sensitivity  -  ' + typename[i])
+        plt.savefig('plots/First_order_sensitivity_|_' + typename[i] + '.png')
+        plt.clf()
+    
+        # Second order
+        plot_index(Si, problem['names'], '2', 'Second order sensitivity  -  ' + typename[i])
+        plt.savefig('plots/Second_order_sensitivity_|_' + typename[i] + '.png')
+        plt.clf()
+    
+        # Total order
+        plot_index(Si, problem['names'], 'T', 'Total order sensitivity  -  ' + typename[i])
+        plt.savefig('plots/Total_order_sensitivity_|_' + typename[i] + '.png')
+        plt.clf()
